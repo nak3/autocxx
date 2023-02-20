1b960d6ca40402449c8dfdcab6abeba08b08f22c
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2023-02-20 11:46:54 +0900

    tmp

--- demo/build.rs
+++ demo/build.rs
@@ -7,7 +7,7 @@
 // except according to those terms.
 
 fn main() -> miette::Result<()> {
-    let path = std::path::PathBuf::from("src");
+    let path = std::path::PathBuf::from("..");
     let mut b = autocxx_build::Builder::new("src/main.rs", [&path]).build()?;
     b.flag_if_supported("-std=c++14").compile("autocxx-demo");
 
