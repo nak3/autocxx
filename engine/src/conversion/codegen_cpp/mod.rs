60c8640fedd6cc2dd287127f20dc86ca6ad6e097
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2023-02-20 11:36:47 +0900

    tmp revert

--- engine/src/conversion/codegen_cpp/mod.rs
+++ engine/src/conversion/codegen_cpp/mod.rs
@@ -58,7 +58,7 @@ impl Header {
             Self::System(name) => format!("#include <{name}>"),
             Self::CxxH => {
                 let prefix = cpp_codegen_options.path_to_cxx_h.as_ref().unwrap_or(&blank);
-                format!("#include \"{prefix}cxxgen.h\"")
+                format!("#include \"{prefix}cxx.h\"")
             }
             Self::CxxgenH => {
                 let prefix = cpp_codegen_options
